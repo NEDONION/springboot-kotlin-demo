766b5df3819e4cf720c0f4b990b0c0e5d0454d9d
NEDONION <nedjiachenghu@gmail.com>
2023-05-15 23:16:36 -0700

    init springboot project with kotlin

---
+++ src/main/kotlin/com/jiacheng/demo/SpringbootKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.jiacheng.springbootkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/jiacheng/springbootkotlindemo/SpringbootKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jiacheng.springbootkotlindemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
