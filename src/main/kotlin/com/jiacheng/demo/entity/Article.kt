9111269b386045c30baa701fb2c01e0dab19b92f
NEDONION <nedjiachenghu@gmail.com>
2023-05-15 23:55:01 -0700

    feat: entity, and utils layer

---
+++ src/main/kotlin/com/jiacheng/demo/entity/Article.kt
@@ -0,0 +1,17 @@
+package com.jiacheng.demo.entity
+
+import com.jiacheng.demo.utils.toSlug
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Article (
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+    var title: String,
+    var headline: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now()
+)
---
+++ src/main/kotlin/com/jiacheng/demo/entity/User.kt
@@ -0,0 +1,16 @@
+package com.jiacheng.demo.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null
+)
+
+
